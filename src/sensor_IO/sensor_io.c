c2e4e32212f548ba10a1c62b8b97f557da5da4bc
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2024-08-10 18:09:59 -0300

    Added fan condition

--- src/sensor_IO/sensor_io.c
+++ src/sensor_IO/sensor_io.c
@@ -1,6 +1,7 @@
 #define TMP 9
 #define RedLed 11
-#define BlueLed 3
+#define BlueLed 5
+#define YellowLed 3
 #define SensorPIR 2
 
 const int LM35 = A0; // Pin that will read the output of the LM35 temperature sensor
@@ -10,13 +11,14 @@ int trigger;
 // Global variable to store the intensity
 int intensity_red_light = 0;
 // Fan intensity
-int intensity_fan = 0;
+int fanSpeed = 0;
 
 void setup()
 {
   // Signal 9 configured as Output
   pinMode(RedLed, OUTPUT);
 	pinMode(BlueLed, OUTPUT);
+  pinMode(YellowLed, OUTPUT);
   pinMode(SensorPIR, INPUT);
 
   Serial.begin(9600); // Sets the speed and starts communication
@@ -44,17 +46,19 @@ void loop()
   {
     intensity_red_light = 255;
   }
-  analogWrite(RedLed, intensity_red_light);
-  
-if (trigger == HIGH) 
-    {
-      // Fan on
-      Serial.println("IS MOVING");
-    }else
-    {
-      // Fan off
-    }
+  analogWrite(RedLed, intensidade_red_light);
 
-    // Wait half a second
-     delay(500);
+  if (trigger == HIGH) 
+  {
+    digitalWrite(BlueLed, HIGH);
+    if (fanSpeed > 0)
+    {
+    	digitalWrite(YellowLed, HIGH);
+    }
+  }else
+  {
+    fanSpeed=0;
+    digitalWrite(BlueLed, LOW);
+    digitalWrite(YellowLed, LOW);
+  }
 }
